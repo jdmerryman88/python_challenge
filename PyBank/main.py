19a888171e708446a7b28639645f6bc7d29d341a
Justin Merryman <justinmerryman@yahoo.com>
2020-11-16 14:55:57 -0600

    Pybank assignment completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -31,21 +31,40 @@ with open(pybank_csv) as csv_file:
             greatest_decrease = change
             worst_month = row[0]
 
+total_months_output = "Total Months: " + str(months)
+output_path = '/Users/justinmerryman/Documents/Rice-BootCamp-Homeworks/python_challenge/PyBank/analysis/analysis.txt'
 
+# Open the file using "write" mode. Specify the variable to hold the contents
+file = open(output_path, 'w',)
+file.write("Financial Analysis \n")
+file.write("------------------------------------- \n")
+file.write(total_months_output)
+file.write("\n")
+file.write("Total: $" + str(average_change))
+file.write("\n")
+file.write("Average_Change: $" + str(average_change / months))
+file.write("\n")
+file.write("Greatest Increase in Profits: " + greatest_month + " ($" + str(greatest_increase) + ")")
+file.write("\n")
+file.write("Greatest Decrease in Profits: "+ worst_month + " ($" + str(greatest_decrease) + ")")
+file.write("\n")
+file.write("")
+file.close()
 
     
     
-    print("Financial Analysis")
-    print("------------------")
-    print("Total Months: " + str(months) )
-    print("Total: $" + str(average_change))
-    print("Average_Change: $" + str(average_change / months))
+print("Financial Analysis")
+print("--------------------------------------")
+print("Total Months: " + str(months) )
+print("Total: $" + str(average_change))
+print("Average_Change: $" + str(average_change / months))
     
-    print("Greatest Increase in Profits: " + greatest_month + " ($" + str(greatest_increase) + ")")
+print("Greatest Increase in Profits: " + greatest_month + " ($" + str(greatest_increase) + ")")
 
-    print("Greatest Decrease in Profits: "+ worst_month + " ($" + str(greatest_decrease) + ")")
-    print(" ")
+print("Greatest Decrease in Profits: "+ worst_month + " ($" + str(greatest_decrease) + ")")
+print(" ")
     
+
 
 
 
