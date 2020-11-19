0298fc43c6fb755709c96d826aba83406c6b1f1e
Justin Merryman <justinmerryman@yahoo.com>
2020-11-18 19:29:27 -0600

    Corrected calculation of average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,19 @@
 import os
 import csv
 
-pybank_csv = os.path.join('Resources/budget_data.csv')
+#pybank_csv = os.path.join('Resources/budget_data.csv')
 
-#pybank_csv = '/Users/justinmerryman/Documents/Rice-BootCamp-Homeworks/python_challenge/PyBank/Resources/budget_data.csv'
+pybank_csv = '/Users/justinmerryman/Documents/Rice-BootCamp-Homeworks/python_challenge/PyBank/Resources/budget_data.csv'
+#
 
 months = 0
 greatest_increase = int(0)
 greatest_decrease = int(0)
-average_change = int(0)
+average_change = []
 greatest_month = "Jan-2020"
 worst_month = "Jan-2020"
+total_change = float(0) 
+total = 0
 
 # Open and read csv
 with open(pybank_csv) as csv_file:
@@ -23,7 +26,10 @@ with open(pybank_csv) as csv_file:
     for row in csv_reader:
         months = months + 1
         change = int(row[1])
-        average_change = average_change + change
+        total = total + change
+        average_change.append(row[1])
+        
+
 
         if change >= greatest_increase:
             greatest_increase = change
@@ -32,11 +38,26 @@ with open(pybank_csv) as csv_file:
         elif change <= greatest_decrease:
             greatest_decrease = change
             worst_month = row[0]
+        
+initial = float(average_change[0])
+difference = 0
+for x in range(len(average_change)):
+    if x  == 0:
+        difference = 0
+    else:
+        difference = float(average_change[x]) - initial
+        total_change = total_change + difference
+        initial = float(average_change[x])
+        print(difference)
+
 
 total_months_output = "Total Months: " + str(months)
-output_path = os.path.join('analysis/analysis.txt')
+output_avg = total_change / (months - 1) 
+output_avg = ("%.2f" % output_avg)
+print(total_change)
+#output_path = os.path.join('analysis/analysis.txt')
 
-#output_path = '/Users/justinmerryman/Documents/Rice-BootCamp-Homeworks/python_challenge/PyBank/analysis/analysis.txt'
+output_path = '/Users/justinmerryman/Documents/Rice-BootCamp-Homeworks/python_challenge/PyBank/analysis/analysis.txt'
 
 # Open the file using "write" mode. Specify the variable to hold the contents
 file = open(output_path, 'w',)
@@ -44,9 +65,9 @@ file.write("Financial Analysis \n")
 file.write("------------------------------------- \n")
 file.write(total_months_output)
 file.write("\n")
-file.write("Total: $" + str(average_change))
+file.write("Total: $" + str(total))
 file.write("\n")
-file.write("Average_Change: $" + str(average_change / months))
+file.write("Average_Change: $" + str(output_avg))
 file.write("\n")
 file.write("Greatest Increase in Profits: " + greatest_month + " ($" + str(greatest_increase) + ")")
 file.write("\n")
@@ -60,8 +81,8 @@ file.close()
 print("Financial Analysis")
 print("--------------------------------------")
 print("Total Months: " + str(months) )
-print("Total: $" + str(average_change))
-print("Average_Change: $" + str(average_change / months))
+print("Total: $" + str(total))
+print("Average_Change: $" + str(output_avg))
     
 print("Greatest Increase in Profits: " + greatest_month + " ($" + str(greatest_increase) + ")")
 
